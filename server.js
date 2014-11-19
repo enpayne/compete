052edfae36c36b822be7d57f83cf8cf1303a4965
Nick Payne <nick.payne89@gmail.com>
2014-11-19 14:22:13 -0800

    change from redis to mongoose, start implementing facebook authentication

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
 var express = require('express');
 var app     = express();
-var redis   = require("redis"), client = redis.createClient();
+var mongoose = require('mongoose');
+var passport = require('passport');
 
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
+require('./app/routes')(app, passport);
+require('./app/user')(app, mongoose);
+
+mongoose.connect('localhost:compete');
 
 app.use(express.static('public'));
-
-app.get('/', function(req, res) {
-    res.sendfile('./public/src/views/index.html');
-});
-
-
-app.get('/api/players', function(req, res) {
-   var players = [
-       {
-           name : "nick",
-           score : 7
-       },
-       {
-           name : "pete",
-           score : 5
-       }
-   ]
-
-    res.json(players);
-});
-
 
 app.listen(8080);
 console.log('Magic happens on 8080');
