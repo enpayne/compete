ec4af00f391726f966fd8ddde59dbca75c461909
Nick Payne <nick.payne89@gmail.com>
2014-12-07 16:28:00 +0100

    eb

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth': {
         'clientID': '1526321890943192', // your App ID
         'clientSecret': '8de5ac00f60b13fe5af27e0cc5b360ad', // your App Secret
-        'callbackURL': 'http://tournamator-env-ks5na5f2wr.elasticbeanstalk.com/auth/facebook/cb'
+        'callbackURL': 'http://localhost:8081/auth/facebook/cb'
     }
 
 };
